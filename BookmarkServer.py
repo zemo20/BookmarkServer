db647592d389754af0897ae23d5669c25e39fd50
zemo20 <zem020@hotmail.com>
2018-02-20 01:10:51 +0200

    changes

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -152,6 +152,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
              self.wfile.write('Didn\'t successfully fetch the long URI')
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
